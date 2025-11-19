6852b2e22dddd73b782280bf024d42b8288b8e98
utsavbalar1231 <utsavbalar1231@gmail.com>
2025-11-20 02:00:43 +0530

    style: apply prettier formatting to config and layout files
    Apply automated prettier formatting (single to double quotes, line
    breaks) to lighthouse config and layout component.
    
    Change-Id: I0e1f2d3c4b5a6f7e8d9c0a1b2c3d4e5f6a7b8c9d
    Signed-off-by: utsavbalar1231 <utsavbalar1231@gmail.com>

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -1,42 +1,42 @@
 export default {
   ci: {
     collect: {
-      staticDistDir: './dist',
+      staticDistDir: "./dist",
       url: [
-        'http://localhost/index.html',
-        'http://localhost/about/index.html',
-        'http://localhost/projects/index.html',
-        'http://localhost/tutorials/index.html',
+        "http://localhost/index.html",
+        "http://localhost/about/index.html",
+        "http://localhost/projects/index.html",
+        "http://localhost/tutorials/index.html",
       ],
       numberOfRuns: 3,
     },
     assert: {
       assertions: {
-        'categories:performance': ['error', { minScore: 0.9 }],
-        'categories:accessibility': ['error', { minScore: 0.95 }],
-        'categories:best-practices': ['error', { minScore: 0.9 }],
-        'categories:seo': ['error', { minScore: 0.9 }],
+        "categories:performance": ["error", { minScore: 0.9 }],
+        "categories:accessibility": ["error", { minScore: 0.95 }],
+        "categories:best-practices": ["error", { minScore: 0.9 }],
+        "categories:seo": ["error", { minScore: 0.9 }],
 
         // WebGL and Canvas effects shouldn't block rendering
-        'largest-contentful-paint': ['warn', { maxNumericValue: 2500 }],
-        'first-contentful-paint': ['warn', { maxNumericValue: 1800 }],
-        'cumulative-layout-shift': ['error', { maxNumericValue: 0.1 }],
-        'total-blocking-time': ['warn', { maxNumericValue: 300 }],
+        "largest-contentful-paint": ["warn", { maxNumericValue: 2500 }],
+        "first-contentful-paint": ["warn", { maxNumericValue: 1800 }],
+        "cumulative-layout-shift": ["error", { maxNumericValue: 0.1 }],
+        "total-blocking-time": ["warn", { maxNumericValue: 300 }],
 
         // Font optimization (DepartureMono)
-        'font-display': 'off', // We use font-display: swap in CSS
+        "font-display": "off", // We use font-display: swap in CSS
 
         // Resource optimization
-        'unused-javascript': 'off', // Astro optimizes this
-        'unused-css-rules': 'off', // Tailwind purges unused CSS
+        "unused-javascript": "off", // Astro optimizes this
+        "unused-css-rules": "off", // Tailwind purges unused CSS
 
         // Modern browser requirements (Chrome 90+, Firefox 88+, Safari 16+)
-        'uses-webp-images': 'off', // We use webp already
-        'modern-image-formats': 'off',
+        "uses-webp-images": "off", // We use webp already
+        "modern-image-formats": "off",
       },
     },
     upload: {
-      target: 'temporary-public-storage',
+      target: "temporary-public-storage",
     },
   },
 };
